f0a02c5c6b01c3b6a53b24a7941affd07b35c208
abid ali <abidali9068@gmail.com>
2018-07-11 03:30:08 +0500

    Product Collection Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 namespace App\Http\Resources\Product;
 
@@ -18,6 +18,7 @@ class ProductCollection extends Resource
             'name' => $this->name,
             
             'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+            'discount' => $this->discount,
 
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) :'No rating yet',
             'href' => ['link' => route('products.show',$this->id)]
